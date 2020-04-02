ae9ad14452bc655648e7a8805e7f7e7288a93db7
euskalvirus <euskalvirus@gmail.com>
2020-04-02 13:42:35 +0200

    JS cambio array denombre a objetoJson con personas

--- Prueba_Alumnos_Profesores/appcliente/js/main2.js
+++ Prueba_Alumnos_Profesores/appcliente/js/main2.js
@@ -7,8 +7,39 @@ console.error('mensaje de error')
 
 var lista = document.getElementById('lista');
 
-let nombres = ['Mary', 'Jenny', 'Jhon', 'Arthur', 'Stephany', 'Naruto', 'Girama'];
+let personas = [{
+        "avatar": "img/avatar1.png",
+        "nombre": "Mary",
+        "sexo": "F"
+    },
+    {
+        "avatar": "img/avatar2.png",
+        "nombre": "Jenny",
+        "sexo": "F"
+    }, {
+        "avatar": "img/avatar3.png",
+        "nombre": "Jhon",
+        "sexo": "M"
+    }, {
+        "avatar": "img/avatar4.png",
+        "nombre": "Arthur",
+        "sexo": "M"
+    },
+    {
+        "avatar": "img/avatar5.png",
+        "nombre": "Stephany",
+        "sexo": "F"
+    }, {
+        "avatar": "img/avatar6.png",
+        "nombre": "Naruto",
+        "sexo": "M"
+    }, {
+        "avatar": "img/avatar7.png",
+        "nombre": "Girama",
+        "sexo": "M"
+    }
+]
 lista.innerHTML = '';
 for (let i = 0; i < 7; i++) {
-    lista.innerHTML += `<li class="list-group-item "><img src="img/avatar${i+1}.png " alt="imagen avatar ">${nombres[i]}</li>`
+    lista.innerHTML += `<li class="list-group-item "><img src=${personas[i].avatar} " alt="imagen avatar ">${personas[i].nombre} </li>`
 }
