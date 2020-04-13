8252ebad344c275607c10f0a1850f1e22f4c7111
euskalvirus <euskalvirus@gmail.com>
2020-04-13 21:00:32 +0200

    agregados todo los metodos CRUD para arraylist

--- Prueba_Alumnos_Profesores/appRest/src/com/ipartek/formacion/api/controller/PersonaController.java
+++ Prueba_Alumnos_Profesores/appRest/src/com/ipartek/formacion/api/controller/PersonaController.java
@@ -1,13 +1,17 @@
 package com.ipartek.formacion.api.controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
@@ -44,10 +48,57 @@ public class PersonaController {
 		return personas;
 	}
 	
+	@GET
+	@Path("/{id}")
+	public Persona getPersona(@PathParam("id") Long id) {	
+		LOGGER.info("getPersona");
+		for (Persona per : personas) {
+			if(per.getId()==id) {return per;}
+		}
+		return null;
+	}
+	
 	@POST
 	public ArrayList<Persona> addPersona(Persona persona){
+		LOGGER.info("addPersona");
 		personas.add(persona);
 		return personas;
+	}
+	
+	@PUT
+	@Path("/{id}")
+	public ArrayList<Persona> modifyPersona(Persona persona,@PathParam("id") Long id){
+		LOGGER.info("modifyPersona");
+		
+		int i = 0;
+		Boolean encontrado = false;
+		while(i<personas.size() && !encontrado) {
+			if(personas.get(i).getId()==id) {
+				personas.set(i, persona);
+				encontrado=true;
+			}
+			i++;
+		}
+		return personas;
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public ArrayList<Persona> deletePersona(@PathParam("id") Long id){
+		LOGGER.info("deletePersona");
+		int i = 0;
+		Boolean encontrado = false;
+		while(i<personas.size() && !encontrado) {
+			if(personas.get(i).getId()==id) {
+				System.out.println("encontrado");
+				personas.remove(i);
+				encontrado=true;
+			}
+			i++;
+		}
+		System.out.println(personas);
+		return personas;
+		
 	}
 	
 }
